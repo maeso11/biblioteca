891c65380312bd96f1b6243d0f5019f84a224e70
Victorse9 <victor.rse97@gmail.com>
2021-01-26 12:00:39 +0100

    Retoques interfaz

--- biblioteca/src/application/InicioController.java
+++ biblioteca/src/application/InicioController.java
@@ -1,5 +1,15 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class InicioController {
-	
+	@FXML private TableView <Libros> libros;
+	@FXML private TableColumn isbn;
+	@FXML private TableColumn titulo;
+	@FXML private TableColumn descripcion;
+	@FXML private TableColumn autor;
+	@FXML private TableColumn editorial;
+	@FXML private TableColumn fechaPublicacion;
 }
--- biblioteca/src/application/Main.java
+++ biblioteca/src/application/Main.java
@@ -13,9 +13,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Inicio.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,900,670);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
